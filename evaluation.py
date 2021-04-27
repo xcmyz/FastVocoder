3cef041d58c45b5207810dd226d333b9944e18f1
xcmyz <xcmyz@outlook.com>
2021-04-27 18:17:20 +0800

    Update evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,3 @@
-from numpy.core.defchararray import decode
 import torch
 import os
 import argparse
@@ -7,27 +6,9 @@ import hparams as hp
 
 from data.utils import parse_path_file
 from model.generator.melgan import MelGANGenerator
+from synthesize import Synthesizer
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-class Synthesizer:
-    def __init__(self, checkpoint_path) -> None:
-        self.model = self.load_model(checkpoint_path)
-
-    def load_model(self, checkpoint_path):
-        model = MelGANGenerator().to(device)
-        model.load_state_dict(
-            torch.load(os.path.join(checkpoint_path),
-                       map_location=torch.device(device))['model'])
-        model.eval()
-        model.remove_weight_norm()
-        return model
-
-    def synthesize(self, mel):
-        with torch.no_grad():
-            est_source = self.model.inference(mel)
-        return est_source
 
 
 def load_data(audio_index_path, mel_index_path, index_list):
@@ -42,7 +23,6 @@ def load_data(audio_index_path, mel_index_path, index_list):
 
 
 if __name__ == "__main__":
-    # Test
     parser = argparse.ArgumentParser()
     parser.add_argument('--checkpoint_path', type=str)
     parser.add_argument('--audio_index_path', type=str, default=os.path.join("dataset", "audio", "eval"))
