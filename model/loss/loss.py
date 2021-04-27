657317a34ce5ebfebd77d39b2532f8c83853aed7
xcmyz <xcmyz@outlook.com>
2021-04-27 10:33:23 +0800

    Add loss function

---
+++ model/loss/loss.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+from .stft_loss import MultiResolutionSTFTLoss
+
+
+class Loss(nn.Module):
+    def __init__(self):
+        super(Loss, self).__init__()
+        self.stft_loss = MultiResolutionSTFTLoss()
+
+    def forward(self, est_source, wav):
+        wav.requires_grad = False
+        assert est_source.size(1) == wav.size(1)
+        sc_loss, mag_loss = self.stft_loss(est_source, wav)
+        stft_loss = sc_loss + mag_loss
+        return stft_loss
