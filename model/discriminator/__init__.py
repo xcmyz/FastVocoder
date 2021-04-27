f41d750f50cb313894feeb224f58cac92c12af7d
xcmyz <xcmyz@outlook.com>
2021-04-27 11:32:31 +0800

    Update init

--- model/discriminator/__init__.py
+++ model/discriminator/__init__.py
@@ -1,3 +1,4 @@
 from .mpd import MultiPeriodDiscriminator
 from .msd import MelGANMultiScaleDiscriminator
 from .mfd import MultiResolutionSTFTDiscriminator
+from .discriminator import Discriminator
