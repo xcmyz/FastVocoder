63472ba8173853f08a5b9f0da75585668f9281c2
xcmyz <xcmyz@outlook.com>
2021-04-27 14:38:28 +0800

    Add module

--- model/generator/modules.py
+++ model/generator/modules.py
@@ -82,3 +82,13 @@ class LastLayer(nn.Module):
         x = self.pad(x)
         x = self.conv(x)
         return x
+
+
+def init_weights(m, mean=0.0, std=0.01):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        m.weight.data.normal_(mean, std)
+
+
+def get_padding(kernel_size, dilation=1):
+    return int((kernel_size*dilation - dilation)/2)
