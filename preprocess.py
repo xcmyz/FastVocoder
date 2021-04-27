ef3021b1eff4dd74ad15d8f292d5a8128a363138
xcmyz <xcmyz@outlook.com>
2021-04-27 11:31:39 +0800

    Add multiprocssing

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,14 @@
+from data.utils import pad
 import os
 import numpy as np
 import data.audio as audio
 
 from tqdm import tqdm
+from functools import partial
+from concurrent.futures import ProcessPoolExecutor
+
+CPU_NUM = 8
+MULTI_PROCESS = True
 
 
 def preprocess(data_path_file, save_path):
@@ -29,5 +35,48 @@ def preprocess(data_path_file, save_path):
     return audio_index, mel_index
 
 
+def kernel(wav_filepath, mel_filepath, new_wav_filepath):
+    try:
+        y = audio.load_wav(wav_filepath, encode=False)
+        mel = audio.melspectrogram(y)
+        np.save(mel_filepath, mel)
+        np.save(new_wav_filepath, y)
+    except Exception:
+        pass
+
+
+def preprocess_multiprocessing(data_path_file, save_path):
+    executor = ProcessPoolExecutor(max_workers=CPU_NUM)
+    futures = []
+    os.makedirs(save_path, exist_ok=True)
+    audio_index = []
+    mel_index = []
+
+    with open(data_path_file, "r") as f:
+        lines = f.readlines()
+        for i in tqdm(range(len(lines))):
+            filename = lines[i]
+            wav_filepath = os.path.join(filename[:-1])
+            wav_filename = filename[:-1].split("/")[-1]
+            mel_filepath = os.path.join(save_path, f"{wav_filename}.mel.npy")
+            new_wav_filepath = os.path.join(save_path, f"{wav_filename}.npy")
+            audio_index.append(new_wav_filepath)
+            mel_index.append(mel_filepath)
+            futures.append(executor.submit(partial(kernel, wav_filepath, mel_filepath, new_wav_filepath)))
+    [future.result() for future in tqdm(futures)]
+    return audio_index, mel_index
+
+
 if __name__ == "__main__":
-    preprocess(os.path.join("dataset", "ljspeech.txt"), os.path.join("dataset", "processed"))
+    audio_index, mel_index = 0, 0
+    if MULTI_PROCESS:
+        audio_index, mel_index = preprocess_multiprocessing(os.path.join("dataset", "ljspeech.txt"), os.path.join("dataset", "processed"))
+    else:
+        audio_index, mel_index = preprocess(os.path.join("dataset", "ljspeech.txt"), os.path.join("dataset", "processed"))
+    with open(os.path.join("dataset", "audio_index_path.txt"), "w", encoding="utf-8") as f:
+        for path in audio_index:
+            f.write(path + "\n")
+
+    with open(os.path.join("dataset", "mel_index_path.txt"), "w", encoding="utf-8") as f:
+        for path in mel_index:
+            f.write(path + "\n")
