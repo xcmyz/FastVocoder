9bfe752d5263b926c5a2a754aafa381e61c1c611
xcmyz <xcmyz@outlook.com>
2021-04-28 22:12:06 +0800

    Update

--- model/loss/loss.py
+++ model/loss/loss.py
@@ -10,14 +10,21 @@ class Loss(nn.Module):
 
     def forward(self, est_source, wav, pqmf=None):
         if pqmf is not None:
-            wav = pqmf.analysis(wav.unsqueeze(1))
-            est_source = est_source.view(-1, est_source.size(2))
-            wav = wav.view(-1, wav.size(2))
-            assert est_source.size(0) == wav.size(0)
-            assert est_source.size(1) == wav.size(1)
-            wav.requires_grad = False
-            sc_loss, mag_loss = self.stft_loss(est_source, wav)
-            stft_loss = sc_loss + mag_loss
+            est_source_sub_band = est_source
+            wav_full_band = wav
+            wav_sub_band = pqmf.analysis(wav_full_band.unsqueeze(1))
+            est_source_full_band = pqmf.synthesis(est_source_sub_band)[:, 0, :]
+            est_source_sub_band = est_source_sub_band.view(-1, est_source_sub_band.size(2))
+            wav_sub_band = wav_sub_band.view(-1, wav_sub_band.size(2))
+            assert est_source_sub_band.size(0) == wav_sub_band.size(0)
+            assert est_source_sub_band.size(1) == wav_sub_band.size(1)
+            wav_full_band.requires_grad = False
+            wav_sub_band.requires_grad = False
+            sc_loss_sub_band, mag_loss_sub_band = self.stft_loss(est_source_sub_band, wav_sub_band)
+            stft_loss_sub_band = sc_loss_sub_band + mag_loss_sub_band
+            sc_loss_full_band, mag_loss_full_band = self.stft_loss(est_source_full_band, wav_full_band)
+            stft_loss_full_band = sc_loss_full_band + mag_loss_full_band
+            stft_loss = (stft_loss_sub_band + stft_loss_full_band) / 2.
             return stft_loss
 
         wav.requires_grad = False
