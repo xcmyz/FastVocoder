bcec6939bdbe97b2c5c8d8b141d69e52f69de9e2
xcmyz <xcmyz@outlook.com>
2021-04-28 16:06:01 +0800

    Fix

--- model/loss/loss.py
+++ model/loss/loss.py
@@ -11,10 +11,13 @@ class Loss(nn.Module):
     def forward(self, est_source, wav, pqmf=None):
         if pqmf is not None:
             wav = pqmf.analysis(wav.unsqueeze(1))
-            print(wav.size(), est_source.size())
             assert est_source.size(2) == wav.size(2)
             wav.requires_grad = False
-            sc_loss, mag_loss = self.stft_loss(est_source, wav)
+            sc_loss, mag_loss = 0., 0.
+            for i in range(est_source.size(1)):
+                sc_loss_, mag_loss_ = self.stft_loss(est_source[:, i, :], wav[:, i, :])
+                sc_loss += sc_loss_
+                mag_loss += mag_loss_
             stft_loss = sc_loss + mag_loss
             return stft_loss
         wav.requires_grad = False
