ce5b9010aa4d54a90a0c10ed44e496577a88325c
xcmyz <xcmyz@outlook.com>
2021-04-29 21:56:07 +0800

    Use hifigan

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ from data.dataset import load_data_to_buffer, collate_fn_tensor
 from data.utils import get_param_num
 
 random.seed(str(time.time()))
-MODEL_NAME = "melgan"
+MODEL_NAME = "hifigan"
 MULTI_BAND = False
 if MODEL_NAME == "multiband-hifigan":
     MULTI_BAND = True
