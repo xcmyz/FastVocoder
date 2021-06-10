43ac85a2ffa198c364ad6af94f330c9cedf6af26
xcmyz <xcmyz@outlook.com>
2021-06-10 19:58:03 +0800

    Fix bug

--- bin/train.py
+++ bin/train.py
@@ -60,7 +60,7 @@ def trainer(model, discriminator,
     # Init
     optimizer.zero_grad()
     discriminator_optimizer.zero_grad()
-    if basis_signal_optimizer is None:
+    if basis_signal_optimizer is not None:
         basis_signal_optimizer.zero_grad()
 
     # Generator Forward
@@ -76,6 +76,7 @@ def trainer(model, discriminator,
     s_l = stft_loss.item()
     stft_loss = hp.lambda_stft * stft_loss
     total_loss = total_loss + stft_loss
+    w_l = 0.
     if weight_loss is not None and current_step <= hp.discriminator_train_start_steps:
         w_l = weight_loss.item()
         total_loss += weight_loss
@@ -188,7 +189,7 @@ def trainer(model, discriminator,
         Now = time.perf_counter()
 
         str1 = f"Epoch [{epoch + 1}/{hp.epochs}], Step [{current_step}/{total_step}]:"
-        str2 = "STFT Loss: {:.6f}, Total Loss: {:.6f};".format(s_l, t_l)
+        str2 = "STFT Loss: {:.6f}, Weight Loss: {:.6f}, Total Loss: {:.6f};".format(s_l, w_l, t_l)
         str3 = "Adversarial Loss: {:.6f}, Discriminator Loss: {:.6f}, Feature Map Loss: {:.6f};".format(a_l, d_l, f_l)
         if scheduler is None:
             str4 = "Current Learning Rate is {:.6f}, discriminator Learning Rate is {:.6f};".format(hp.learning_rate, hp.learning_rate_discriminator)
@@ -311,6 +312,7 @@ def run(args):
     logger.info(f'Number of TTS Parameters: {num_param}')
 
     # Optimizer and loss
+    basis_signal_optimizer = None
     if not args.mixprecision:
         if args.model_name == "basis-melgan":
             optimizer = Adam(model.melgan.parameters(), lr=args.learning_rate, eps=1.0e-6, weight_decay=0.0)
@@ -387,7 +389,7 @@ def run(args):
                                  shuffle=True,
                                  collate_fn=collate_fn_tensor,
                                  drop_last=True,
-                                 num_workers=0,
+                                 num_workers=4,
                                  prefetch_factor=2,
                                  pin_memory=True)
     total_step = hp.epochs * len(training_loader) * hp.batch_expand_size
@@ -410,6 +412,7 @@ def run(args):
                 mel = db["mel"].float().to(device)
                 wav = db["wav"].float().to(device)
                 mel = mel.contiguous().transpose(1, 2)
+                weight = None
                 if "weight" in db:
                     weight = db["weight"].float().to(device)
                 clock_1_e = time.perf_counter()
@@ -445,8 +448,11 @@ def run(args):
                         valid_mel = valid_mel.contiguous().transpose(1, 2)
                         valid_wav = valid_batch["wav"].float().to(device)
                         with torch.no_grad():
-                            valid_est_source = model(valid_mel)
-                            valid_stft_loss = vocoder_loss(valid_est_source, valid_wav, pqmf=pqmf)
+                            if args.model_name == "basis-melgan":
+                                valid_est_source, _ = model(valid_mel)
+                            else:
+                                valid_est_source = model(valid_mel)
+                            valid_stft_loss, _ = vocoder_loss(valid_est_source, valid_wav, pqmf=pqmf)
                             valid_loss_all += valid_stft_loss.item()
                         if ii == hp.valid_num:
                             break
