f08a6c71c6a64f7491662297ec25dcef7a49d4ad
xcmyz <xcmyz@outlook.com>
2021-06-10 16:00:32 +0800

    Sync training script

--- train.sh
+++ train.sh
@@ -4,12 +4,11 @@ dataset_audio_valid=$3
 dataset_mel=$4
 dataset_mel_valid=$5
 model_name=$6
-multi_band=$7
+config=$7
 use_scheduler=$8
 mixprecision=${9:-'0'}
-config=${10}
-checkpoint_path=${11:-'""'}
-restore_step=${12:-'0'}
+checkpoint_path=${10:-'""'}
+restore_step=${11:-'0'}
 if [ "$mixprecision" -eq "1" ]; then
     echo "mix precision training"
 fi
@@ -22,9 +21,8 @@ CUDA_VISIBLE_DEVICES=$GPU python3 bin/launcher.py \
     --audio_index_valid_path $dataset_audio_valid \
     --mel_index_valid_path $dataset_mel_valid \
     --model_name $model_name \
-    --multi_band $multi_band \
+    --config $config \
     --use_scheduler $use_scheduler \
     --mixprecision $mixprecision \
     --checkpoint_path $checkpoint_path\
-    --restore_step $restore_step \
-    --config $config
+    --restore_step $restore_step
