4996c7e57f4662dee4961c5609c22c79cb4bf809
xcmyz <xcmyz@outlook.com>
2021-06-13 16:30:05 +0800

    Update for pssd

--- dataset/biaobei.py
+++ dataset/biaobei.py
@@ -3,4 +3,5 @@ import os
 data_path = os.path.join("..", "BZNSYP", "Wave")
 with open("BZNSYP.txt", "w", encoding="utf-8") as f:
     for filename in os.listdir(data_path):
-        f.write(os.path.abspath(os.path.join(data_path, filename))+"\n")
+        if filename[0] != ".":
+            f.write(os.path.abspath(os.path.join(data_path, filename))+"\n")
