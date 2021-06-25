650eeaf1a507cf94bcb6835c6dce2f0b3ef6fcdc
xcmyz <xcmyz@outlook.com>
2021-06-25 17:03:16 +0800

    Add testing

--- bin/launcher.py
+++ bin/launcher.py
@@ -5,7 +5,7 @@ sys.path.append(".")
 
 
 if __name__ == "__main__":
-    from bin import run_train, run_preprocess, run_synthesizer, run_publisher
+    from bin import run_train, run_preprocess, run_synthesizer, run_publisher, run_test
     MODE = os.getenv("MODE")
     if MODE == "train":
         run_train()
@@ -15,3 +15,5 @@ if __name__ == "__main__":
         run_synthesizer()
     elif MODE == "publish":
         run_publisher()
+    elif MODE == "test":
+        run_test()
