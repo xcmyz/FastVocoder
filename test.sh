b0a018e692cd7d580777e207652331e23fe4d670
xcmyz <xcmyz@outlook.com>
2021-06-28 16:38:42 +0800

    Update script

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 checkpoint=$1
-model_name=$2
-config=$3
+filelist=$2
+model_name=$3
+config=$4
 
 export MODE=test
 
 CUDA_VISIBLE_DEVICES=-1 python3 bin/launcher.py \
     --checkpoint $checkpoint \
     --model_name $model_name \
-    --config $config
+    --config $config \
+    --file_path $filelist
